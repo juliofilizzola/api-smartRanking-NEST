34f65348fa66d6d68aeb334b22e5a49838d98cd6
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 19:33:00 -0300

    🔨: criando categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import { CategoriesModule } from './categories/categories.module';
 import 'dotenv/config'
 
 const options = {
@@ -13,7 +14,7 @@ const options = {
 const password = process.env.DB_PASS;
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb+srv://devSmart:${password}@smartranking.f9xnj.mongodb.net/ranking?retryWrites=true&w=majority`, options), PlayersModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://devSmart:${password}@smartranking.f9xnj.mongodb.net/ranking?retryWrites=true&w=majority`, options), PlayersModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
