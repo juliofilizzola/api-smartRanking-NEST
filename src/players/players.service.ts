57ea308a8372baf3925d4676fecf9af17bbd6585
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 10:54:44 -0300

    🔨: services finalizado

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { createPlayerDto } from './dtos/createPlayer.dto';
 import { updatePlayerDtos } from './dtos/updatePlayer.dto';
 import { player } from './interface/player.interface';
@@ -7,29 +9,39 @@ import { player } from './interface/player.interface';
 export class PlayersService {
   private readonly logger = new Logger(PlayersService.name);
 
-  async createPlayer(player: createPlayerDto) {
+  constructor(@InjectModel('players') private readonly playerModel: Model<player>) {}
+
+
+  async createPlayer(player: createPlayerDto): Promise<player> {
     this.logger.log(`create Player: ${player}`);
-  }~
+    const createPlayer = new this.playerModel(player);
+    return createPlayer.save();
+  }
 
-  async getPlayer() {
+  async getPlayer(): Promise<player[]> {
+    return this.playerModel.find().exec();
 
   }
 
-  async getPlayerById(id: number) {
-
+  async getPlayerById(id: number): Promise<player> {
+    const response = await this.playerModel.findOne({ _id: id });
+    return response;
   }
 
-  async getPlayerByName(name: string) {
-
+  async getPlayerByName(name: string): Promise<player> {
+    const response = await this.playerModel.findOne({ name: name });
+    return response;
   }
 
-  async updatePlayer(id: number, upPlayer: updatePlayerDtos) {
-    
+  async updatePlayer(id: number, upPlayer: updatePlayerDtos): Promise<player> {
+    const responseUpdate = await this.playerModel.findOneAndUpdate({_id: id},
+      {$set: upPlayer}).exec();
+    return responseUpdate;
   }
 
-  async deletedPlayer(id: number) {
-
+  async deletedPlayer(id: number): Promise<any> {
+    const responseDelete = await this.playerModel.deleteOne({_id: id});
+    return responseDelete;
   }
-
 
 }
