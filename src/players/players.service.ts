944e73780ecd773d45db71ad3bcaba1643ce5af7
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 10:26:41 -0300

    🔨: basico do service

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { createPlayerDto } from './dtos/createPlayer.dto';
+import { updatePlayerDtos } from './dtos/updatePlayer.dto';
+import { player } from './interface/player.interface';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private readonly logger = new Logger(PlayersService.name);
+
+  async createPlayer(player: createPlayerDto) {
+    this.logger.log(`create Player: ${player}`);
+  }~
+
+  async getPlayer() {
+
+  }
+
+  async getPlayerById(id: number) {
+
+  }
+
+  async getPlayerByName(name: string) {
+
+  }
+
+  async updatePlayer(id: number, upPlayer: updatePlayerDtos) {
+    
+  }
+
+  async deletedPlayer(id: number) {
+
+  }
+
+
+}
