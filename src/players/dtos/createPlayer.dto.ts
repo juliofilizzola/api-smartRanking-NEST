c01d127614abbce9e37888cf0114b1d644817ef5
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 09:45:59 -0300

    🔨: criando dtos

---
+++ src/players/dtos/createPlayer.dto.ts
@@ -0,0 +1,5 @@
+export class createPlayerDto {
+  readonly phoneCel: string;
+  readonly email: string;
+  readonly name: string;
+}
--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { createPlayerDto } from './dtos/createPlayer.dto';
+import { PlayersService } from './players.service';
 
-@Controller('players')
-export class PlayersController {}
+@Controller('api/v1/players')
+export class PlayersController {
+  constructor(private readonly playerServices: PlayersService) {}
+
+  @Post('/new')
+  async createNewPlayer(@Body() newPlayer: createPlayerDto) {
+
+  }
+
+  @Put('/updateRegister')
+  async updatePlayer(@Body()setUpdate ) {
+
+  }
+
+}
