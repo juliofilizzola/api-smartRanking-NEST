9c9a9c6ed31930a03b3c84446ea15d2458aa1127
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 09:22:40 -0300

    🔨: criando services e controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PlayersService } from './players/players.service';
+import { PlayersController } from './players/players.controller';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [PlayersController],
+  providers: [PlayersService],
 })
 export class AppModule {}
