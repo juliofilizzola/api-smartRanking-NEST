06bc27db3d0dc1101d321e642efc513bd2f4a15f
Filizzola <juliofilizzola@hotmail.com>
2022-01-30 10:49:08 -0300

    🔨: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players/players.service';
 import { PlayersController } from './players/players.controller';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayersModule } from './players/players.module';
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false
+  // useCreateIndex: true,
+}
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://devSmart:smartDev@smartranking.f9xnj.mongodb.net/ranking?retryWrites=true&w=majority', options), PlayersModule],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
---
+++ src/players/players.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayerSchema } from '../schema/player.schema';
+import { PlayersController } from './players.controller';
+import { PlayersService } from './players.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'players', schema: PlayerSchema }])],
+  controllers: [PlayersController],
+  providers: [PlayersService]
+})
+export class PlayersModule {}
