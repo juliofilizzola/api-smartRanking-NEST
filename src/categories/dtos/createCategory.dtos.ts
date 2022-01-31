63dbb8db6a0702d672af19a070bd2efcfcc07cae
Filizzola <juliofilizzola@hotmail.com>
2022-01-31 07:15:20 -0300

    🔨: criando validações de categories

--- src/categories/dtos/createCategory.dtos.ts
+++ src/categories/dtos/createCategory.dtos.ts
@@ -1,5 +1,15 @@
+import { IsEmail, isEmpty, IsEmpty, IsString } from "class-validator";
+
 export class createCategoryDto {
+  @IsEmpty()
+  @IsString()
   readonly category: string;
+
+  @IsEmpty()
+  @IsEmail()
   readonly email: string;
+  
+  @IsEmpty()
+  @IsString()
   readonly name: string;
 }
