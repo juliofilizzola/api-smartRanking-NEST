0457a2578d03998830b3f2ffd83d6b881e22c003
Filizzola <juliofilizzola@hotmail.com>
2022-02-04 10:59:28 -0300

    🔨: fazendo modificações nos arquivo para melhor adquação do código

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { createCategoryDto } from './dtos/createCategory.dtos';
 import { categories } from './interfaces/categories.interface';
@@ -14,4 +14,28 @@ export class CategoriesController {
     return result;
   }
 
+
+  @Get("/AllCategories")
+  async getCategories(): Promise<categories[]> {
+    const result = await this.categoriesServices.getCategories();
+    if (!result) {
+      throw new NotFoundException("Ainda não tem nenhuma categoria registrada.");
+    }
+    return result;
+  }
+
+  @Get("/categoriesID/:id")
+  async getCategoriesById(@Param('id') id: string): Promise<categories> {
+    const result = await this.categoriesServices.getCategoriesById(id);
+    if (!result) {
+      throw new NotFoundException("Não existe essa categoria ainda.");
+    }
+    return result;
+  }
+
+  @Post('/:categories/player/:player')
+  async setAttributePlayer(@Param('categories') categories: string, @Param('player') player: string): Promise<any> {
+    const result = await this.categoriesServices.setAttributePlayer(categories, player);
+    return result;
+  }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { player } from 'src/players/interface/player.interface';
 import { createCategoryDto } from './dtos/createCategory.dtos';
 import { categories } from './interfaces/categories.interface';
 
@@ -8,12 +9,30 @@ import { categories } from './interfaces/categories.interface';
 export class CategoriesService {
 
   constructor(
-    @InjectModel('category') private readonly categoryModel: Model<categories>
+    @InjectModel('category') private readonly categoryModel: Model<categories>,
+    @InjectModel('players') private readonly playerModel: Model<player>
     ) {}
 
   async createCategories(categories: createCategoryDto): Promise<categories> {
     const category = new this.categoryModel(categories);
     const result = await category.save();
     return result;
+  }
+
+  async getCategories(): Promise<categories[]> {
+    const categories = await this.categoryModel.find();
+    return categories;
+  }
+
+  async getCategoriesById(id: string): Promise<categories> {
+    const category = await this.categoryModel.findById(id);
+    return category;
+  }
+
+  async setAttributePlayer(categories: string, player: string) {
+    const getPlayer = await this.playerModel.findOne({ name: player });
+    if (!getPlayer) {
+      throw new NotFoundException("player not exist");
+    }
   }
 }
