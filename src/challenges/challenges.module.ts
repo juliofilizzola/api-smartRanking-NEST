dfa957011acca273f890c6896d3b8447a2b815db
Filizzola <juliofilizzola@hotmail.com>
2022-02-07 11:16:32 -0300

    ✨: criando controller

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChallengesService } from './challenges.service';
+import { ChallengesController } from './challenges.controller';
 
 @Module({
-  providers: [ChallengesService]
+  providers: [ChallengesService],
+  controllers: [ChallengesController]
 })
 export class ChallengesModule {}
