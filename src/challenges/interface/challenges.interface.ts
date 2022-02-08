66f4d723876f38d8c14aea74e39c8e55d7432616
Filizzola <juliofilizzola@hotmail.com>
2022-02-08 10:06:57 -0300

    ✨: modificando a interface

--- src/challenges/interface/challenges.interface.ts
+++ src/challenges/interface/challenges.interface.ts
@@ -1,9 +1,14 @@
 import { Document } from 'mongoose';
 
 import { player } from "src/players/interface/player.interface";
+import { ChallengesStatus } from '../enum/challengesStatus.enum';
 
 export interface challenges extends Document {
+  dateChallenge: Date,
+  status: ChallengesStatus,
   request: String,
+  RequestDate: Date,
+  ResponseDate: Date,
   category: String, 
   players: Array<player>,
   matches: String
