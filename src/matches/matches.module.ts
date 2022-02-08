db8705a0b9cd3507855c8908691d8e24171793d6
Filizzola <juliofilizzola@hotmail.com>
2022-02-08 11:11:01 -0300

    ✨: modificando modules e schema do matches

--- src/matches/matches.module.ts
+++ src/matches/matches.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MatchesSchema } from 'src/schema/matches.schema';
+import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 
 @Module({
-  providers: [MatchesService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'matches', schema: MatchesSchema }]),
+  ],
+  providers: [MatchesService],
+  controllers: [MatchesController]
 })
 export class MatchesModule {}
