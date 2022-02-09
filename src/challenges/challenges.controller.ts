4e020adaa5e506760ef83f13317b9a6eee5a038b
Filizzola <juliofilizzola@hotmail.com>
2022-02-09 18:55:38 -0300

    🔨: Criando função de post e get no controlle

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ChallengesService } from './challenges.service';
+import { CreateChallengesDto } from './dtos/createChallenges.dto';
 
 @Controller('challenges')
-export class ChallengesController {}
+export class ChallengesController {
+  constructor(private readonly ChallengesServices: ChallengesService) {}
+
+  @Post('new')
+  async createNewChallenges(@Body() createChallenges: CreateChallengesDto) {
+    const result = await this.ChallengesServices.newChallenges(createChallenges);
+    return result;
+  }
+
+  @Get('/get')
+  async getChallenges() {
+    const result = await this.ChallengesServices.getChallenges();
+    return result;
+  }
+}
